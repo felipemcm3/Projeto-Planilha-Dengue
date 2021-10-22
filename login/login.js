6821b521e154a8aa5ecd983157470636cd61f393
Felipe <felipemcm3@gmail.com>
2021-10-22 10:18:57 -0300

    dengue0.2

--- login/login.js
+++ login/login.js
@@ -2,9 +2,10 @@ function entrou(){
     var usuario = document.getElementById('usuario')
     var senha = document.getElementById('senha')
     var res = document.getElementById('alerta')
+    var entrar = document.getElementById('entrar')
     if(usuario.value.length == 0 || senha.value.length == 0){
         res.innerHTML = 'Informe os dados'
     }else{
-        window.alert('tudo ok')
+        window.location.href = "../inicio/inicio.html"
     }
 }
