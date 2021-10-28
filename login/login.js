d4fe8dc246a8ed3d76bdb1513da4bdc4771568b6
Felipe <felipemcm3@gmail.com>
2021-10-28 09:08:25 -0300

    login

--- login/login.js
+++ login/login.js
@@ -1,12 +1,12 @@
 function entrou(){
-    var usuario = document.getElementById('usuario')
-    var senha = document.getElementById('senha')
-    var res = document.getElementById('alerta')
-    var entrar = document.getElementById('entrar')
+    var usuario = document.getElementById('usuario');
+    var senha = document.getElementById('senha');
+    var res = document.getElementById('alerta');
+    var entrar = document.getElementById('entrar');
     if(usuario.value.length == 0 || senha.value.length == 0){
-        res.innerHTML = 'Informe os dados'
+        res.innerHTML = 'Informe os dados';
     }else{
-        window.location.href = "../inicio/inicio.html"
+        window.location.href = "../inicio/inicio.html";
         export default usuario;
     }
 }
